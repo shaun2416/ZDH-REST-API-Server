76a9ca3f2f3808c144704c0a46bf6e320bbd0f02
ZS\sc31079 <shaunak.chakraborty@zs.com>
2023-03-26 18:23:20 +0530

    API server files

---
+++ auth.py
@@ -0,0 +1,25 @@
+import cryptography
+import jwt
+
+ISSUER = 'sample-auth-server'
+
+with open('public.pem', 'rb') as f:
+  public_key = f.read()
+
+def verify_access_token(access_token):
+  try:
+    print("verify_access_token method...")
+    decoded_token = jwt.decode(access_token.encode(), public_key,
+                               issuer = ISSUER,
+                               algorithms = ['RS256'])
+    print(f"Decoded token: {decoded_token}")
+
+
+  except (jwt.exceptions.InvalidTokenError,
+          jwt.exceptions.InvalidSignatureError,
+          jwt.exceptions.InvalidIssuerError,
+          jwt.exceptions.ExpiredSignatureError) as e:
+          print(e)
+          return False
+
+  return True
