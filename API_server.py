f3da34da387123d26e83a41e02e969d2631ed0b6
Shaunak Chakraborty <shaunak.chakraborty2416@gmail.com>
2025-11-22 10:02:02 +0530

    modified response

--- API_server.py
+++ API_server.py
@@ -345,7 +345,7 @@ def get_user():
                     "long_name": "1600",
                     "short_name": "1600",
                     "types": [
-                        "street_number"
+                        {"key" : "street_number"}
                     ]
                 },
                 {
@@ -359,39 +359,39 @@ def get_user():
                     "long_name": "Mountain View",
                     "short_name": "Mountain View",
                     "types": [
-                        "locality",
-                        "political"
+                        {"key": "locality"},
+                        {"key": "political"}
                     ]
                 },
                 {
                     "long_name": "Santa Clara County",
                     "short_name": "Santa Clara County",
                     "types": [
-                        "administrative_area_level_2",
-                        "political"
+                        {"key": "administrative_area_level_2"},
+                        {"key": "political"}
                     ]
                 },
                 {
                     "long_name": "California",
                     "short_name": "CA",
                     "types": [
-                        "administrative_area_level_1",
-                        "political"
+                        {"key": "administrative_area_level_1"},
+                        {"key": "political"}
                     ]
                 },
                 {
                     "long_name": "United States",
                     "short_name": "US",
                     "types": [
-                        "country",
-                        "political"
+                        {"key":"country"},
+                        {"key":"political"}
                     ]
                 },
                 {
                     "long_name": "94043",
                     "short_name": "94043",
                     "types": [
-                        "postal_code"
+                        {"key":"postal_code"}
                     ]
                 }
             ],
@@ -429,53 +429,53 @@ def get_user():
                     "long_name": "1600",
                     "short_name": "1600",
                     "types": [
-                        "street_number"
+                        {"key":"street_number"}
                     ]
                 },
                 {
                     "long_name": "Amphitheatre Parkway",
                     "short_name": "Amphitheatre Pkwy",
                     "types": [
-                        "route"
+                        {"key":"route"}
                     ]
                 },
                 {
                     "long_name": "Mountain View",
                     "short_name": "Mountain View",
                     "types": [
-                        "locality",
-                        "political"
+                        {"key":"locality"},
+                        {"key":"political"}
                     ]
                 },
                 {
                     "long_name": "Santa Clara County",
                     "short_name": "Santa Clara County",
                     "types": [
-                        "administrative_area_level_2",
-                        "political"
+                        {"key":"administrative_area_level_2"},
+                        {"key":"political"}
                     ]
                 },
                 {
                     "long_name": "California",
                     "short_name": "CA",
                     "types": [
-                        "administrative_area_level_1",
-                        "political"
+                        {"key":"administrative_area_level_1"},
+                        {"key":"political"}
                     ]
                 },
                 {
                     "long_name": "United States",
                     "short_name": "US",
                     "types": [
-                        "country",
-                        "political"
+                        {"key":"country"},
+                        {"key":"political"}
                     ]
                 },
                 {
                     "long_name": "94043",
                     "short_name": "94043",
                     "types": [
-                        "postal_code"
+                        {"key":"postal_code"}
                     ]
                 }
             ],
